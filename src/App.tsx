57e89d9e14e29b6d4ee6a5b89d90da5f004cf932
Guilherme Queiroz <guilher.queiroz@gmail.com>
2025-03-17 14:08:10 -0300

    feat: Add page de começar quiz

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Quiz from "./pages/Quiz";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/quiz" element={<Quiz />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Quiz/index.tsx
@@ -0,0 +1,11 @@
+import "./index.scss";
+
+const Quiz = () => {
+  return (
+    <div>
+      <h1>Começar</h1>
+    </div>
+  );
+};
+
+export default Quiz;
